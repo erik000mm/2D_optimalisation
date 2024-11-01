a05ccf31632c3813e516e80c28f438102e69abbc
Erik <103986793+erik000mm@users.noreply.github.com>
2024-11-01 10:24:22 +0100

    nelder mead done

--- nelder_mead.py
+++ nelder_mead.py
@@ -1,57 +1,112 @@
 import numpy as np
 
 
-def find_max_and_index(lst):
-    max_value = max(lst)
-    max_index = len(lst) - 1 - lst[::-1].index(max_value)
-    
-    return max_value, max_index
-
-
 def func(x):
     return ((x[0]-1)**2 + (x[1]-2)**2)
 
 
-def nelder_mead(points):
-    # ordering by function
-    calculated_points = []
+def nelder_mead(points, max_iterations=100, tolerance=1e-6):
+    f_points = []
 
+    # Calculating points based on function f
     for point in points:
-        calculated_points.append([func(point[0]), point[1]])
+        f_points.append([func(point[0]), point[1]])
 
-    max_point, max_index = find_max_and_index(calculated_points)
-    worst_number = calculated_points[max_index]
-    points.pop(max_index)
-    
-    # calculate center without worst point
-    x_sum = sum(pair[0] for pair in points)
+    for iteration in range(max_iterations):
+        # Sort the points
+        sorted_f_points = sorted(f_points, key=lambda x: x[0])
 
-    center = x_sum/2
+        if sorted_f_points[-1][0] - sorted_f_points[0][0] < tolerance:
+            print(f"Termination condition met after {iteration} iterations.")
+            break
 
-    # try reflexion c + alpha * (c - A/B/C)
-    xr = center + 1 * (center - max_point[0])
+        # Worst point based on f
+        worst_f_point = sorted_f_points[-1]
 
-    f_xr = func(xr)
+        # Worst point
+        worst_point = next((sub for sub in points if sub[1] == worst_f_point[1]), None)
 
-    second_best = min(points[0])
+        filtered_array = [item for item in points if item[1] != worst_point[1]]
 
-    # If then try contraction
-    if f_xr > worst_number:
-        xc = center + 0.5 * (max_point[0] - center)
-        f_xc = func(xc)
-        """
-        if f_xc > worst_number:
-            print()
+        center = sum(pair[0] for pair in filtered_array)/2
+
+        # Try reflexion 
+        xr = center + 1 * (center - worst_point[0])
+        f_xr = func(xr)
+
+        new_points = []
+
+        if f_xr < sorted_f_points[0][0]:
+            # try expansion
+            xe = center + 2 * (xr - center)
+            f_xe = func(xe)
+            
+            # If expanded point better than reflected
+            if f_xe < f_xr:
+                filtered_array.append([xe, worst_point[1]])
+            else:
+                filtered_array.append([xr, worst_point[1]])
+
+        elif f_xr < sorted_f_points[1][0] and f_xr >= sorted_f_points[0][0]:
+            # accept reflexion
+            filtered_array.append([xr, worst_point[1]])
+        elif f_xr >= worst_f_point[0]:
+            # try contraction
+            xc = center + 0.5 * (worst_point[0] - center)
+            f_xc = func(xc)
+
+            if f_xc < f_xr:
+                filtered_array.append([xc, worst_point[1]])
         else:
-            print("point better")
-            points.append(xc)
-        """
+            # Filter the best
+            best_value = sorted_f_points[0]
+            best_point = next((sub for sub in points if sub[1] == sorted_f_points[0][1]), None)
 
+            missing_points = [item[1] for item in sorted_f_points if item != best_value]
 
+            sorted_f_points.pop(0)
+            farray = sorted_f_points
 
+            for index, point in enumerate(farray):
+                xs = np.array([0, 0])
+                xs = best_point[0] + 0.5 * (point[0] - best_point[0])
+                new_points.append([xs, missing_points[index]])
 
-    print()
+            new_points.append(best_point)
+
+        if new_points:
+            points = new_points
+        else:
+            points = filtered_array
+
+        f_points = []
+
+        # Calculating points based on function f
+        for point in points:
+            f_points.append([func(point[0]), point[1]])
+
+        print()
+
+    return points
 
 points = [[np.array([0, 0]), 'A'], [np.array([2, 0]), 'B'], [np.array([0, 3]), 'C']]
 
-nelder_mead(points=points)
+new_points = nelder_mead(points=points)
+
+def find_lowest_point(points):
+    lowest_point = None
+    min_distance = float('inf')
+    
+    for point, label in points:
+        distance = np.linalg.norm(point)
+        if distance < min_distance:
+            min_distance = distance
+            lowest_point = (point, label)
+    
+    return lowest_point
+
+
+lowest_point = find_lowest_point(new_points)
+
+# Output the result
+print(f"Lowest Point: {lowest_point[0]}, Objective function minimum value: {func(lowest_point[0])}")
